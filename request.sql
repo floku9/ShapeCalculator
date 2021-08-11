71fac8a34cd9d42d68ffb25c0af3053051f6b0b8
DESKTOP-LSANH0R\Александр <floku9@outlook.com>
2021-08-11 14:20:11 +0300

    Update SQL Request

--- request.sql
+++ request.sql
@@ -1,3 +1,3 @@
-SELECT Products.name as Name, Category.name as Category FROM Products 
+SELECT Products.name as productName, Category.name as category FROM Products 
 LEFT JOIN Category ON Products.id = Category.productId
-Order by Name
+Order by productName
